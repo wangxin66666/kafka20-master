a7194292c24367a7983e2bda48e7f68cdbc42c3c
王鑫 <291143840@qq.com>
2021-12-26 16:43:29 +0800

    kafka

---
+++ src/test/java/com/mashibing/kafka/AppTest.java
@@ -0,0 +1,20 @@
+package com.mashibing.kafka;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
